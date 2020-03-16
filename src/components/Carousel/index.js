6f2c7b93ec5d23e96434178e100bffe29006315c
Eli Selkin <eli.j.selkin@gmail.com>
2020-03-15 21:42:55 -0700

    oops

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,27 +3,47 @@ import { makeStyles } from "@material-ui/core/styles";
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
 import ChevronRight from "@material-ui/icons/ChevronRight";
 import Paper from "../../assets/Paperbg.jpg";
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   carouselitem: {
     backgroundImage: `url(${Paper})`,
     position: "relative",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    alignContent: "center",
-    width: "calc(100% - 40px)",
-    margin: "20px",
-    height: "calc(100vh - 350px)"
+    display: "grid",
+    paddingTop: "15px",
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+      marginTop: "5px",
+      gridTemplateColumns: "50% 50%",
+      gridTemplateRows: "auto auto auto", // image, arrows, text
+      gridTemplateAreas: `
+        "image image"
+        "text text"
+        "arrowleft arrowright";
+      `
+    },
+    [theme.breakpoints.up("md")]: {
+      height: "calc(60vh - 40px)",
+      width: "100%",
+      gridTemplateColumns:
+        "calc(5vw - 10px) 2.5vw 60vw 1vw 22vw 2.5vw calc(5vw - 10px)",
+      gridTemplateRows: "auto",
+      marginTop: "20px",
+      gridTemplateAreas: `"arrowleft . image .  text . arrowright";`
+    }
   },
   image: {
-    height: "90%",
-    marginRight: "20px",
-    padding: "20px"
+    padding: "10px",
+    gridArea: "image",
+    width: "calc(100% - 20px)",
+    maxWidth: "700px",
+    maxHeight: "calc(100% - 40px)"
   },
   captionHolder: {
-    width: "30%",
-    height: "calc(100vh - 450px)",
-    overflowY: "hidden"
+    gridArea: "text",
+    padding: "10px",
+    [theme.breakpoints.up("md")]: {
+      height: "100%",
+      overflow: "hidden"
+    }
   },
   caption: {
     width: "100%",
@@ -31,15 +51,20 @@ const useStyles = makeStyles({
     height: "100%"
   },
   captionParagraph: {
-    marginBottom: "10px"
+    marginBottom: "8px"
   },
-  arrow: {
-    margin: "5%",
-    width: "5%",
-    height: "20%",
-    fontSize: "64px"
+  arrowleft: {
+    gridArea: "arrowleft",
+    fontSize: "64px",
+    textAlign: "center"
+  },
+  arrowright: {
+    gridArea: "arrowright",
+    fontSize: "64px",
+    textAlign: "center"
   },
   quote: {
+    marginTop: "10px",
     width: "100%",
     textAlign: "center",
     WebkitAlignContent: "center"
@@ -53,7 +78,7 @@ const useStyles = makeStyles({
       display: "inline"
     }
   }
-});
+}));
 
 const Carousel = ({ items, quote }) => {
   const [index, setIndex] = useState(0);
@@ -63,19 +88,19 @@ const Carousel = ({ items, quote }) => {
     return (
       <div key={item.key} className={classes.carouselitem}>
         <div
-          className={classes.arrow}
+          className={classes.arrowleft}
           onClick={() => {
             if (index > 0) setIndex(index - 1);
           }}
         >
           <ChevronLeft fontSize="inherit" />
         </div>
-        <img src={item.image} className={classes.image} alt={item.alt}/>
+        <img src={item.image} className={classes.image} alt={item.alt} />
         <div className={classes.captionHolder}>
           <div className={classes.caption}>{item.caption(classes)}</div>
         </div>
         <div
-          className={classes.arrow}
+          className={classes.arrowright}
           onClick={() => {
             if (index < items.length - 1) setIndex(index + 1);
           }}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,41 +1,67 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useState } from "react";
 import Cork from "./Cork";
 import Shadows from "./Shadows";
 import Pages from "./Pages";
 import Boxes from "./Boxes";
 import Pin from "./Pin";
+import Menu from "./Menu";
 import { withRouter } from "react-router-dom";
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   nav: {
     width: "100%",
-    height: "200px",
+    [theme.breakpoints.down("md")]: {
+      height: "90px",
+      alignItems: "center",
+      alignContent: "center",
+      textAlign: "-webkit-center"
+    },
+    [theme.breakpoints.up("md")]: {
+      height: "200px"
+    },
     margin: "0px",
     padding: "0px",
     position: "relative",
     top: 0
   }
-});
+}));
 
 const Nav = function({ history }) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
   const classes = useStyles();
-  const [active, setActive] = useState("about");
+  console.log(history)
+  const initial = history.location.pathname.split("/")[1] || "about"
+  const [active, setActive] = useState(initial);
   const [hover, setHover] = useState(null);
   return (
-    <div className={classes.nav}>
-      <Cork />
-      <Shadows active={active} hover={hover} />
-      <Pages active={active} hover={hover} />
-      <Pin active={active} hover={hover} />
-      <Boxes
-        active={active}
-        hover={hover}
-        setActive={setActive}
-        setHover={setHover}
-        history={history}
-      />
-    </div>
+    <>
+      <div
+        className={classes.nav}
+        style={{ display: matches ? "none" : "block" }}
+      >
+        <Cork />
+        <Shadows active={active} hover={hover} />
+        <Pages active={active} hover={hover} />
+        <Pin active={active} hover={hover} />
+        <Boxes
+          active={active}
+          hover={hover}
+          setActive={setActive}
+          setHover={setHover}
+          history={history}
+        />
+      </div>
+      <div
+        className={classes.nav}
+        style={{ display: matches ? "block" : "none" }}
+      >
+        <Cork />
+        <Menu active={active} setActive={setActive} />
+      </div>
+    </>
   );
 };
 
