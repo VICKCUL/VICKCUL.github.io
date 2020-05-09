9344d450363b61ecd89695449dab21afadd35c2f
Eli Selkin <eli.j.selkin@gmail.com>
2020-05-08 20:11:04 -0700

    build

--- src/components/About/quote.js
+++
@@ -1,4 +0,0 @@
-export default {
-  quoteText: "To be, or not to be, that is the question...",
-  quoteBy: "William Shakespeare"
-};
--- src/components/Personal/quote.js
+++
@@ -1,4 +0,0 @@
-export default {
-  quoteText: "If music be the food of love, play on;",
-  quoteBy: "William Shakespeare"
-};
